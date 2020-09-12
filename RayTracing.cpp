7c6afa0bcc79607e0356e57849c8efcf5d883df3
Oscar Davies <oscar.william.davies@outlook.com>
2020-09-12 16:04:33 +0100

    Ray Reflection

--- RayTracing.cpp
+++ RayTracing.cpp
@@ -9,19 +9,39 @@
 const int ViewWidth = 10;
 const float ViewSteps = 0.01f;
 
+Vec3 CalculateRayColour(Ray* R, Sphere Spheres[], int Reflections = 1) {
+    Vec3 Color(0, 0, 0);
+    float HitDistance = -1.0f, THit;
+
+    for (int i = 0; i < 3; i++) {
+
+        THit = Spheres[i].IntersectionDistance(R);
+
+        if (THit != -1.0f && (THit < HitDistance || HitDistance==-1.0f)) {
+            HitDistance = THit;
+
+            Color = Spheres[i].GetColor()*(1.0f/Reflections);
+
+            Ray* ReflectedRay = Spheres[i].PointNormal(Spheres[i].IntersectionPoint(R, HitDistance), R);
+
+            if (Reflections<10) 
+                Color += CalculateRayColour(ReflectedRay, Spheres, Reflections + 1);
+        }
+
+    }
+    return Color;
+}
+
+
 int main(int argc, char** argv)
 {
-    std::cout << "Hello World!\n";
+    Sphere Spheres[3]{ Sphere(), Sphere(), Sphere() };
 
-    Sphere S;
-    S.radius = 4;
-
-    Sphere S2;
-    S2.radius = 4;
-    S2.SetCenter(Vec3(3,1,0));
+    Spheres[0].radius = 4; Spheres[0].SetCenter(Vec3(-4, -3, 4)); Spheres[0].SetColor(Vec3(255, 0, 0));
+    Spheres[1].radius = 4; Spheres[1].SetCenter(Vec3(4, -2, -1)); Spheres[1].SetColor(Vec3(0, 255, 0));
+    Spheres[2].radius = 4; Spheres[2].SetCenter(Vec3(0, 5, 2)); Spheres[2].SetColor(Vec3(0, 0, 255));
 
     int i = 0;
-    //bool grid[441];
 
     unsigned char* rgb = (unsigned char*) malloc((2*ViewWidth/ViewSteps)*(2*ViewWidth/ViewSteps)*3);
 
@@ -30,20 +50,13 @@ int main(int argc, char** argv)
         if (x >= ViewWidth) { x = -ViewWidth; y += ViewSteps; }
         R.Direction = Vec3(x, y, 20);
 
-        //grid[i] = S.Intersects(&R);
+        Vec3 Color = CalculateRayColour(&R, Spheres);
 
-        if (S2.Intersects(&R)) {
-            rgb[i * 3] = 0;
-            rgb[(i * 3) + 1] = 255;
-            rgb[(i * 3) + 2] = 0;
-        }
-
-        if (S.Intersects(&R)) {
-            rgb[i * 3] = 255;
-            rgb[(i * 3) + 1] = 0;
-            rgb[(i * 3) + 2] = 0;
-        }
+        rgb[i * 3] = Color.x;
+        rgb[i * 3 + 1] = Color.y;
+        rgb[i * 3 + 2] = Color.z;
     }
 
     stbi_write_png("image.png", 2*ViewWidth/ViewSteps, 2*ViewWidth/ViewSteps, 3, rgb, 0);
 }
+
--- SceneObject.h
+++ SceneObject.h
@@ -6,15 +6,10 @@ public:
 	Vec3 Origin = Vec3(0,0,-20), Direction;
 };
 
-class Colour {
-public:
-	float R = 0, G = 0, B = 0;
-};
-
 class SceneObject {
 public:
 	Vec3 Center;
-	Colour Colour;
+	Vec3 Colour;
 
 	bool Intersects(Ray* Ray);
 };
@@ -24,13 +19,8 @@ public:
 	float radius = 0;
 
 	bool Intersects(Ray* Ray) {
-		Vec3 oc = Ray->Origin - Center;
 
-		float a = Ray->Direction.dot(Ray->Direction),
-			b = 2 * oc.dot(Ray->Direction),
-			c = oc.dot(oc) - radius * radius;
-
-		return (b * b - 4 * a * c) > 0;
+		return IntersectionDistance(Ray)>=0;
 	}
 
 	float IntersectionDistance(Ray* Ray) {
@@ -46,12 +36,38 @@ public:
 			return -1.0;
 		}
 		else {
-			return (-b - sqrt(discriminant)) / (2.0 * a);
+			float d = (-b - sqrt(discriminant)) / (2.0 * a);
+			if (d < 0) return -1.0;
+			else return d;
 		}
+	}
+
+	Vec3 IntersectionPoint(Ray* Ray, float Distance) {
+		return Ray->Origin + (Ray->Direction * Distance);
+	}
+
+	Ray* PointNormal(Vec3 Point, Ray* oRay) {
+
+		Ray* nRay = new Ray();
+
+		nRay->Direction = -(2 * (oRay->Direction.dot(Point - Center)) * (Point - Center) - oRay->Direction);
+		nRay->Origin = Point;
+
+		return nRay;
+	}
+
+	void SetColor(Vec3 Color) {
+		this->Colour = Color;
+	}
+
+	Vec3 GetColor() {
+		return Colour;
 	}
 
 	void SetCenter(Vec3 NewPos) {
 		Center = NewPos;
 	}
 };
+
+
 
