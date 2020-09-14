95720728b05a7ab7c2e56a94cd4603d3b1d8968d
Oscar Davies <oscar.william.davies@outlook.com>
2020-09-14 11:05:10 +0100

    Minor Code Rearrange

--- RayTracing.cpp
+++ RayTracing.cpp
@@ -1,70 +1,9 @@
-#include <iostream>
-#include <thread>
-
-#include "SceneObject.h"
-
-#include "Sphere.h"
-#include "Triangle.h"
-
-#include "SceneObjectList.h"
+#include "Rendering.h"
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
-
-const int ViewWidth = 10;
-const float ViewSteps = 0.01f;
-const int ViewWS = 2000;
-
-Vec3 CalculateRayColour(Ray* R, List* Objs, int Reflections = 1) {
-	Vec3 Color(0, 0, 0);
-	float HitDistance = -1.0f, THit, rhit;
-
-	Item* I = Objs->Head;
-	SceneObject* O;
-
-	while (I != 0x0) {
-		O = (SceneObject*)I->Obj;
-
-		THit = O->IntersectionDistance(R);
-
-		if (THit != -1.0f) {
-			rhit = O->CorrectDistance(R, THit);
-
-			if (rhit < HitDistance || HitDistance == -1.0f) {
-				HitDistance = rhit;
-
-				Color = O->Colour * (1.0f / Reflections);
-
-				if (Reflections < 20) {
-					Ray* ReflectedRay = O->PointNormal(O->IntersectionPoint(R, HitDistance), R);
-					Color += CalculateRayColour(ReflectedRay, Objs, Reflections + 1);
-					delete ReflectedRay;
-				}
-				
-			}
-		}
-
-		I = I->Next;
-	}
-	return Color;
-}
-
-void RenderRow(float y, int i, List* Objs, unsigned char* rgb) {
-	Ray R;
-	Vec3 Color;
-
-	for (float x = -ViewWidth; x < ViewWidth; x += ViewSteps, i++) {
-		R.Direction = Vec3(x, y, 20);
-
-		Color = CalculateRayColour(&R, Objs);
-
-		rgb[i * 3] = Color.x;
-		rgb[i * 3 + 1] = Color.y;
-		rgb[i * 3 + 2] = Color.z;
-	}
-}
 
 int main(int argc, char** argv)
 {
@@ -76,19 +15,7 @@ int main(int argc, char** argv)
 	Objs.Add(new Triangle(Vec3(0,-3,2), Vec3(3,0,-5), Vec3(-3,0,4), Vec3(255, 0, 0)));
 	Objs.Add(new Triangle(Vec3(-10, -8, 0), Vec3(-10, 8, 0), Vec3(00, 0, 20), Vec3(0, 120, 0)));
 
-	int i = 0;
-
-	unsigned char* rgb = (unsigned char*)malloc(12 * pow(ViewWidth / ViewSteps, 2));
-
-	std::thread Threads[ViewWS];
-	std::thread tThread;
-
-	for (float y = -ViewWidth; y < ViewWidth; y += ViewSteps, i++) {
-		tThread = std::thread(RenderRow, y, i * ViewWS, &Objs, rgb);
-		Threads[i].swap(tThread);
-	}
-
-	for (i = 0; i < ViewWS; i++) Threads[i].join();
+	unsigned char* rgb = RenderScene(&Objs);
 
 	stbi_write_png("image.png", 2 * ViewWidth / ViewSteps, 2 * ViewWidth / ViewSteps, 3, rgb, 0);
 }
---
+++ Rendering.h
@@ -0,0 +1,80 @@
+#pragma once
+#include "SceneObject.h"
+
+#include "Sphere.h"
+#include "Triangle.h"
+
+#include "List.h"
+
+#include <thread>
+
+const int ViewWidth = 10;
+const float ViewSteps = 0.01f;
+const int ViewWS = 2000;
+
+Vec3 CalculateRayColour(Ray* R, List* Objs, int Reflections = 1) {
+	Vec3 Color(0, 0, 0);
+	float HitDistance = -1.0f, THit, rhit;
+
+	Item* I = Objs->Head;
+	SceneObject* O;
+
+	while (I != 0x0) {
+		O = (SceneObject*)I->Obj;
+
+		THit = O->IntersectionDistance(R);
+
+		if (THit != -1.0f) {
+			rhit = O->CorrectDistance(R, THit);
+
+			if (rhit < HitDistance || HitDistance == -1.0f) {
+				HitDistance = rhit;
+
+				Color = O->Colour * (1.0f / Reflections);
+
+				if (Reflections < 20) {
+					Ray* ReflectedRay = O->PointNormal(O->IntersectionPoint(R, HitDistance), R);
+					Color += CalculateRayColour(ReflectedRay, Objs, Reflections + 1);
+					delete ReflectedRay;
+				}
+
+			}
+		}
+
+		I = I->Next;
+	}
+	return Color;
+}
+
+void RenderRow(float y, int i, List* Objs, unsigned char* rgb) {
+	Ray R;
+	Vec3 Color;
+
+	for (float x = -ViewWidth; x < ViewWidth; x += ViewSteps, i++) {
+		R.Direction = Vec3(x, y, 20);
+
+		Color = CalculateRayColour(&R, Objs);
+
+		rgb[i * 3] = Color.x;
+		rgb[i * 3 + 1] = Color.y;
+		rgb[i * 3 + 2] = Color.z;
+	}
+}
+
+unsigned char* RenderScene(List* Objs) {
+	int i = 0;
+
+	unsigned char* rgb = (unsigned char*)malloc(12 * pow(ViewWidth / ViewSteps, 2));
+
+	std::thread Threads[ViewWS];
+	std::thread tThread;
+
+	for (float y = -ViewWidth; y < ViewWidth; y += ViewSteps, i++) {
+		tThread = std::thread(RenderRow,y, i * ViewWS, Objs, rgb);
+		Threads[i].swap(tThread);
+	}
+
+	for (i = 0; i < ViewWS; i++) Threads[i].join();
+
+	return rgb;
+}
