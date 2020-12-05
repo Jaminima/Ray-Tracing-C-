6f14de9e29e892327cc1cec05389c8320a63d69e
Oscar Davies <oscar.william.davies@outlook.com>
2020-12-05 16:28:31 +0000

    Camera

--- Const.h
+++ Const.h
@@ -1,7 +1,7 @@
 #pragma once
-#include "vec3.h"
+#include "Camera.h"
 
-const unsigned int px = 1500, py = 1500;
+const unsigned int px = 1000, py = 1000;
 const unsigned int px_half = px / 2, py_half = px / 2;
 
-Vec3 mainCamera = Vec3(-10, 5, -10);
+Camera mainCamera;
--- Rendering.h
+++ Rendering.h
@@ -4,6 +4,9 @@
 
 #include "Const.h"
 
+#include <chrono>
+using namespace std::chrono;
+
 #include <Windows.h>
 #include <amp.h>
 using namespace concurrency;
@@ -43,11 +46,19 @@ Color RenderRay(Ray r, array_view<Sphere, 1> spheres) restrict(amp) {
 	return c;
 }
 
-Color RenderPixel(index<2> idx, array_view<Sphere,1> spheres, Vec3 Camera) restrict(amp) {
+Color RenderPixel(index<2> idx, array_view<Sphere,1> spheres, Camera cam) restrict(amp) {
 	float vx = (idx[1] / (float)px_half) - 1,
 		vy = (idx[0] / (float)px_half) - 1;
 
-	Ray r(Camera,Vec3(vx, vy, 1));
+	vx *= cam.fov;
+	vy *= cam.fov;
+
+	Vec3 Direction = Vec3(vx, vy, 1);
+
+	Direction += cam.Angle;
+	Direction.normalise();
+
+	Ray r(cam.Position,Direction);
 	return RenderRay(r,spheres);
 }
 
@@ -56,23 +67,24 @@ Sphere spheres[3]{ Sphere(), Sphere(), Sphere() };
 
 Color* RenderScene(Color* rgb) {
 
+	spheres[0].Center = Vec3(5, 0, 0);
 	spheres[0].color = Color(0, 255, 0);
 
-	spheres[1].Center = Vec3(5, 5, 5);
+	spheres[1].Center = Vec3(5, 0, 5);
 	spheres[1].color = Color(255, 0, 0);
 
-	spheres[2].Center = Vec3(-5, -5, 5);
+	spheres[2].Center = Vec3(-5, 0, -5);
 	spheres[2].color = Color(0,0,255);
 
 	array_view<Color, 2> ColorView(px, py, rgb);
 	array_view<Sphere, 1> SphereView(3, spheres);
 
-	Vec3 Camera = Vec3(mainCamera);
+	Camera cam = mainCamera;
 
 	parallel_for_each(
 		ColorView.extent,
 		[=](index<2> idx) restrict(amp) {
-			ColorView[idx[0]][idx[1]] = RenderPixel(idx, SphereView, Camera);
+			ColorView[idx[0]][idx[1]] = RenderPixel(idx, SphereView, cam);
 		}
 	);
 
@@ -90,15 +102,19 @@ void drawFrame()
 }
 
 void triggerReDraw() {
-	mainCamera.x += 0.01f;
-	spheres[0].Center.z+= 0.01f;
+	//mainCamera.Angle.x -= 0.001f;
+	//spheres[0].Center.z+= 0.01f;
+		
+	high_resolution_clock::time_point before = high_resolution_clock::now();
 
-	DWORD start = timeGetTime();
 	RenderScene(rgb);
-	DWORD end = timeGetTime();
 
-	printf_s("Frame took %d ms IE %d FPS\n", (end-start), 1000/(end-start));
 	glutPostRedisplay();
+
+	high_resolution_clock::time_point after = high_resolution_clock::now();
+	duration<double> ns = after - before;
+
+	printf_s("Frame took %d ns\n", ns);
 }
 
 void SetupFrame(int argc, char** argv){
