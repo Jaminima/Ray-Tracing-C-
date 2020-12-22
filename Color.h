ebbe2219855dbbdb7b4866678f77a2c330b48351
Oscar Davies <oscar.william.davies@outlook.com>
2020-12-22 15:39:42 +0000

    Fix Color Bit Shifts
    Turns out, i had had Red and Blue the wrong way around

--- Color.h
+++ Color.h
@@ -5,45 +5,45 @@ struct Color {
 public:
 	unsigned int rgba = 0x0F000000;
 
-	void SetR(unsigned int r) restrict(amp, cpu) { rgba = rgba | ((r & 0xFF) << 16); }
+	void SetR(unsigned int r) restrict(amp, cpu) { rgba = rgba | ((r & 0xFF)); }
 	void SetG(unsigned int g) restrict(amp, cpu) { rgba = rgba | ((g & 0xFF) << 8); }
-	void SetB(unsigned int b) restrict(amp, cpu) { rgba = rgba | ((b & 0xFF)); }
+	void SetB(unsigned int b) restrict(amp, cpu) { rgba = rgba | ((b & 0xFF) << 16); }
 
-	unsigned int GetR()restrict(amp, cpu) { return (rgba >> 16) & 0xFF; }
+	unsigned int GetR()restrict(amp, cpu) { return (rgba) & 0xFF; }
 	unsigned int GetG()restrict(amp, cpu) { return (rgba >> 8) & 0xFF; }
-	unsigned int GetB()restrict(amp, cpu) { return rgba & 0xFF; }
+	unsigned int GetB()restrict(amp, cpu) { return (rgba >> 16) & 0xFF; }
 
 	Vec3 GetRGB() restrict(amp, cpu) { return Vec3(GetR(), GetG(), GetB()); }
 
 	Color(unsigned int r, unsigned int g, unsigned int b) restrict(amp, cpu) {
-		rgba |= ((r & 0xFF) << 16) | ((g & 0xFF) << 8) | ((b & 0xFF));
+		rgba |= ((b & 0xFF) << 16) | ((g & 0xFF) << 8) | ((r & 0xFF));
 	}
 
 	Color operator+(Color c) restrict(amp, cpu) {
-		unsigned int r1 = (rgba >> 16) & 0xFF,
+		unsigned int b1 = (rgba >> 16) & 0xFF,
 			g1 = (rgba >> 8) & 0xFF,
-			b1 = rgba & 0xFF;
+			r1 = rgba & 0xFF;
 
-		unsigned int r2 = (c.rgba >> 16) & 0xFF,
+		unsigned int b2 = (c.rgba >> 16) & 0xFF,
 			g2 = (c.rgba >> 8) & 0xFF,
-			b2 = c.rgba & 0xFF;
+			r2 = c.rgba & 0xFF;
 
 		return Color(r1 + r2, g1 + g2, b1 + b2);
 	}
 
 	Color operator * (float f) restrict(amp, cpu) {
-		unsigned int r = (rgba >> 16) & 0xFF,
+		unsigned int b = (rgba >> 16) & 0xFF,
 			g = (rgba >> 8) & 0xFF,
-			b = rgba & 0xFF;
+			r = rgba & 0xFF;
 
 		return Color(r * f, g * f, b * f);
 	}
 
 	Color operator * (Vec3 f) restrict(amp, cpu) {
-		unsigned int r = (rgba >> 16) & 0xFF,
+		unsigned int b = (rgba >> 16) & 0xFF,
 			g = (rgba >> 8) & 0xFF,
-			b = rgba & 0xFF;
+			r = rgba & 0xFF;
 
-		return Color(r * f.z, g * f.y, b * f.x);
+		return Color(r * f.x, g * f.y, b * f.z);
 	}
 };
--- Rendering.h
+++ Rendering.h
@@ -1,6 +1,5 @@
 #pragma once
 #include "RayRendering.h"
-#include "QuickSort.h"
 
 using namespace concurrency::fast_math;
 
