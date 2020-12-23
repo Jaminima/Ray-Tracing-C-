1c2112544e0068c5a76f70d06a336d3443ba6915
Oscar Davies <oscar.william.davies@outlook.com>
2020-12-23 11:07:08 +0000

    Fix Screen Flashing

--- Window.h
+++ Window.h
@@ -26,12 +26,10 @@ Concurrency::completion_future pendingFrameCopy;
 void triggerReDraw() {
 	framesInSec++;
 
-	pendingFrameCopy = rgb[LockedBuffer].synchronize_async();
+	pendingFrameCopy = rgb[!LockedBuffer].synchronize_async();
 
 	RenderScene(rgb[!LockedBuffer]);
 	FXAA(rgb[!LockedBuffer]);
-
-	glutPostRedisplay();
 
 	if (clock() - startTime >= 1000) {
 		printf_s("You averaged %d fps\n", framesInSec);
@@ -39,7 +37,7 @@ void triggerReDraw() {
 		startTime = clock();
 	}
 
-	//OrderCamera();
+	glutPostRedisplay();
 
 	LockedBuffer = !LockedBuffer;
 
