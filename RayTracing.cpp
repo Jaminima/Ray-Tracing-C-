dd83e6feaddfa5a4fb098040ae02f8cec2b8ad04
Oscar Davies <oscar.william.davies@outlook.com>
2021-05-21 19:17:07 +0100

    Cube Mesh & Pre Defined Colours

--- RayTracing.cpp
+++ RayTracing.cpp
@@ -34,12 +34,34 @@ int main(int argc, char** argv)
 	lights[1].colormul = Vec3(0.8f, 0.0f, 0.8f);
 
 	Triangle* t = new Triangle[]{
-			Triangle(Vec3(0, 0, 0), Vec3(0, 1, 0), Vec3(0, 1, 1), Color(255, 0, 255)),
-			Triangle(Vec3(0, 0, 0), Vec3(0, 0, 1), Vec3(0, 1, 1), Color(0, 255, 0))
+		//Front
+			Triangle(Vec3(0, 0, 0), Vec3(0, 1, 0), Vec3(0, 1, 1), Custom1),
+			Triangle(Vec3(0, 0, 0), Vec3(0, 0, 1), Vec3(0, 1, 1), Custom1),
+
+		//Back
+			Triangle(Vec3(1, 0, 0), Vec3(1, 1, 0), Vec3(1, 1, 1), Custom1),
+			Triangle(Vec3(1, 0, 0), Vec3(1, 0, 1), Vec3(1, 1, 1), Custom1),
+
+		//Left
+			Triangle(Vec3(0, 0, 0), Vec3(0, 1, 0), Vec3(1, 1, 0), Custom1),
+			Triangle(Vec3(0, 0, 0), Vec3(1, 0, 0), Vec3(1, 1, 0), Custom1),
+
+		//Right
+			Triangle(Vec3(0, 0, 1), Vec3(0, 1, 1), Vec3(1, 1, 1), Custom1),
+			Triangle(Vec3(0, 0, 1), Vec3(1, 0, 1), Vec3(1, 1, 1), Custom1),
+
+		//Bottom
+			Triangle(Vec3(0, 0, 0), Vec3(1, 0, 0), Vec3(1, 0, 1), Custom1),
+			Triangle(Vec3(0, 0, 0), Vec3(0, 0, 1), Vec3(1, 0, 1), Custom1),
+
+		//Top
+			Triangle(Vec3(0, 1, 0), Vec3(1, 1, 0), Vec3(1, 1, 1), Custom1),
+			Triangle(Vec3(0, 1, 0), Vec3(0, 1, 1), Vec3(1, 1, 1), Custom1),
+
 	};
 
 	for (unsigned int i = 0;i < totalSceneObjects;i++) {
-		sceneObjects[i].ImportTriangles(t, 2, sceneTriangles);
+		sceneObjects[i].ImportTriangles(t, 10, sceneTriangles);
 		sceneObjects[i].OuterCollider = Sphere(15.0f, sceneObjects[i].ApproxPosition(sceneTriangles));
 		sceneObjects[i].Position = Vec3(rand() % 40 - 20, rand() % 40 - 20, rand() % 40 - 20);
 	}
